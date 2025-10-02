d75447069af6cb1880e372f8f511d07b49aba599
Hallie Johnson <hejohnson130@gmail.com>
2025-10-02 00:35:29 +0000

    assignment 7

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/say", async (req, res) => {
+	const keyword = req.query.keyword || "";
+	const response = await axios.get(
+		`https://9m3rc54lzj.execute-api.us-east-2.amazonaws.com/say`,
+		{ params: { keyword } } // sends ?keyword=<keyword>
+	);
+	res.json(response.data);
+});
+
+app.get("/", (req, res) => {
+	res.send("Assignment 07 API running!");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
